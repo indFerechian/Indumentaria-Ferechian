f7ddfbbd8e19dc92ee00f0cddf9815b724142346
David Rodriguez <rdavid1986@outlook.com>
2023-11-27 17:18:29 -0300

    test

---
+++ brasil.js
@@ -0,0 +1,243 @@
+const products = [
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./images/azulado.jpg",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 1,
+        category: "brasil"
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 2
+        
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 3
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 4
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 5
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 6
+        
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 7
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 8
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 9
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 10
+        
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 11
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 12
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 13
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 14
+        
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 15
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 16
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 17
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 18
+        
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 19
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 20
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 21
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 22
+        
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 23
+    },
+    {
+        title: "products",
+        description: "este es el producto",
+        img: "./public/products/product1",
+        whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        productId: 24
+    }
+];
+
+function renderProducts() {
+    const contentProducts = document.getElementById("contentProduct");
+
+    products.forEach((product, index) => {
+        // Agregar condición para filtrar por categoría "brasil"
+        if (product.category !== "brasil") {
+            return; // Omitir este producto si no es de la categoría "brasil"
+        }
+
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+        productDiv.dataset.index = index;
+
+        // Configurar el fondo del div directamente
+        productDiv.style.backgroundImage = `url('${product.img}')`;
+        productDiv.style.backgroundSize = 'cover';
+        productDiv.style.backgroundPosition = 'center';
+        productDiv.style.backgroundRepeat = 'no-repeat';
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.style.opacity = 0;
+
+        const title = document.createElement("h2");
+        title.textContent = product.title;
+
+        const description = document.createElement("p");
+        description.textContent = product.description;
+
+        const category = document.createElement("p");
+        category.href = product.category;
+
+        const whatsapp = document.createElement("a");
+        whatsapp.href = product.whatsapp;
+
+
+        const whatsappButton = document.createElement("img");
+        whatsappButton.src = './Images/logowhatsapp.webp'; // Coloca aquí la URL del logotipo de WhatsApp
+        whatsappButton.alt = "WhatsApp";
+        whatsappButton.style.width = "40px";  
+        whatsappButton.style.height = "40px";
+
+        const divWhatsapp = document.createElement("div");
+        // Agregar elementos al div de productos
+        productDiv.appendChild(title);
+        productDiv.appendChild(description);
+        productDiv.appendChild(category);
+        whatsapp.appendChild(whatsappButton);
+        divWhatsapp.appendChild(whatsapp);
+        productDiv.appendChild(divWhatsapp);
+
+        // Agregar el div del producto al contenedor principal
+        contentProducts.appendChild(productDiv);
+
+        // Agregar un manejador de eventos al div del producto para cambiar el estado del checkbox
+        productDiv.addEventListener("click", () => {
+            window.location.href = `./product.html?productId=${index}`;
+            console.log(`Clic en el producto ${index}`);
+            // Puedes agregar aquí la lógica adicional que necesites
+        });
+    });
+}
+
+// Llamada a la función renderProducts
+document.addEventListener("DOMContentLoaded", renderProducts);
---
+++ product.js
@@ -0,0 +1,207 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener el productId de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get("productId");
+
+    // Obtener el producto correspondiente al productId
+    const product = getProductById(productId);
+
+    // Renderizar el producto
+    renderProduct(product);
+});
+
+// Función para obtener el producto por su id
+function getProductById(productId) {
+    // 'products' debería ser accesible desde aquí o debes cargarlo nuevamente
+    const products = [
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./images/azulado 2.png",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        },
+        {
+            title: "products",
+            description: "este es el producto",
+            img: "./public/products/product1",
+            whatsapp: 'https://api.whatsapp.com/send?phone=1133112388=Hola,%20quiero%20saber%20más%20sobre%20este%20producto.',
+        }
+    ];
+
+    return products[productId];
+}
+
+// Función para renderizar el producto
+function renderProduct(product) {
+    const productContainer = document.getElementById("productContainer");
+
+    productContainer.style.backgroundImage = `url('${product.img}')`;
+    productContainer.style.backgroundSize = "contain";
+    productContainer.style.backgroundRepeat = "no-repeat";
+    productContainer.style.backgroundPosition = "center";
+
+    const title = document.createElement("h2");
+    title.textContent = product.title;
+    title.style.height = "50px"; // Establece la altura a 20px
+
+    const description = document.createElement("p");
+    description.textContent = product.description;
+    description.style.height = "50px"; // Establece la altura a 20px
+    
+    const category = document.createElement("p");
+    category.textContent = product.category;
+
+    const whatsapp = document.createElement("a");
+    whatsapp.href = product.whatsapp;
+
+    const whatsappButton = document.createElement("img");
+    whatsappButton.src = "./images/logowhatsapp.webp"; // Reemplaza con la URL correcta
+    whatsappButton.alt = "WhatsApp";
+    whatsappButton.style.width = "50px"; // Establece el ancho a 20px
+    whatsappButton.style.height = "50px"; // Establece la altura a 20px
+
+    // Agregar elementos al contenedor del producto
+    productContainer.appendChild(title);
+    productContainer.appendChild(description);
+    whatsapp.appendChild(whatsappButton);
+    productContainer.appendChild(whatsapp);
+}
